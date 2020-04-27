9638a8ea25387b66f9d3aa94f923a3e1b9a6b7c2
Alicia Cardete <alicia.escritos@gmail.com>
2020-04-27 17:33:59 +0200

    Zoom images

--- front-page.php
+++ front-page.php
@@ -1,8 +1,7 @@
 <?php get_header(); ?>
-    <main>
         <div data-scroll >
 
-            <section class="panel introBlock smContainer" data-color="pinkLight">
+            <section class="smContainer introBlock panel" data-color="pinkLight">
                 <div class="textBlock">
                     <h1 class="hero">Hello</h1>
                     <p class="paragraph-big">Welcome to my portfolio. My name is Alicia Cardete and I work as a designer. </p>
@@ -10,11 +9,9 @@
                 <span class="scrollMe"></span>
             </section>
     
-            <div class="content">
-            <section class="panel projectsBlock bgContainer" data-color="pinkMedium">
-                <div class="projectsBlock--container">
-                    <h3 class="tertiaryTitle">Selected Works</h3>
-                    <div class="projects">
+            <section class="bgContainer projectsBlock panel" data-color="pinkMedium">
+                <h3 class="tertiaryTitle">Selected Works</h3>
+                <div class="projectsBody">
                             <!-- Creamos la array y definimos los argumentos personalizados -->
                             <!-- Siempre hay que definir el tipo y el número de post que queremos por página -->
                             <!-- Después de la array, creamos una nueva variable con la nueva WP_Query, que será a la que llamaremos  -->
@@ -27,35 +24,29 @@
                             while ( $proyectos->have_posts() ) : $proyectos->the_post(); ?>
                             <?php $thumbnail = get_the_post_thumbnail_url()?>
                                 
-                            <div class="content__item project-item">
+                            <div class="project">
                                 <a href="<?php the_permalink(); ?>">
-                                    <span class="content__item-number">01</span>
-                                    <div class="content__item-imgwrap">
-                                        <div class="content__item-img" style="background-image:url(<?= $thumbnail ?>)"></div>
+                                    <span>01</span>
+                                    <div class="imageWrap">
+                                        <img class="innerImage" src= "<?= $thumbnail ?>" data-scroll-zoom>
                                     </div>
-                                    <div class="content__item-deco"></div>
-                                    <h2 class="content__item-title secondaryTitle"><?php echo get_the_title()?></h2>
-                                    <figcaption class="content__item-description"><?php echo get_the_excerpt()?></figcaption>
+                                    <h2 class="secondaryTitle"><?php echo get_the_title()?></h2>
+                                    <figcaption><?php echo get_the_excerpt()?></figcaption>
                                 </a> 
                             </div>
                             <!-- Tenemos que hacer un wp_reset_postdata() para asegurarnos que vuelve a coger los posts generales -->
                             <?php endwhile; wp_reset_postdata();?>
                         </div>
-                    </div>
-                    
-                    
-                
-            </section>
-            </div>
-            
 
-            <section class="panel contactBlock smContainer" data-color="pinkLight">
+            </section>
+            
+            <section class="smContainer contactBlock panel" data-color="pinkLight">
                 <adress class="textBlock">
                     <p class="primaryTitle">Say hello</p>
                     <a href="mailto:name@email.com" class="paragraph-big">aliciacdt.job@gmail.com</a>
                 </adress>
             </section>
-
         </div>
-    </main> 
+
+
 <?php get_footer(); ?>
